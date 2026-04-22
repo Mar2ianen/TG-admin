dc662922df63a9fa338903677d846f3141068f04
Mar2ianen <mishail.chechulin@gmail.com>
2026-04-22 08:44:23 +0300

    Restore Cargo crate manifest and library entry

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod app;
+pub mod audit;
+pub mod config;
+pub mod event;
+pub mod host_api;
+pub mod observability;
+pub mod parser;
+pub mod scheduler;
+pub mod shutdown;
+pub mod storage;
+pub mod tg;
+pub mod unit;
+
+pub use app::Application;
+pub use config::AppConfig;
