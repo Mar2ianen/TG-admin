36640c54910213c6c58c14483ff7cae182321b3e
Mar2ianen <mishail.chechulin@gmail.com>
2026-04-23 22:44:06 +0300

    Extract host API history methods

--- src/host_api.rs
+++ src/host_api.rs
@@ -14,6 +14,7 @@ mod contract;
 mod ctx;
 mod db;
 mod error;
+mod history;
 mod ml;
 mod validation;
 
@@ -35,8 +36,8 @@ pub(crate) use validation::{
     MAX_JOB_DELAY_DAYS, apply_user_patch, duration_to_chrono, execution_mode_label,
     required_capability, storage_error, to_rfc3339, user_patch_from_increment,
     validate_audit_find_request, validate_event, validate_job_schedule_request, validate_kv_entry,
-    validate_kv_key, validate_msg_by_user_request, validate_msg_window_request, validate_non_empty,
-    validate_user_id, validate_user_incr_request, validate_user_patch,
+    validate_kv_key, validate_non_empty, validate_user_id, validate_user_incr_request,
+    validate_user_patch,
 };
 
 #[derive(Debug, Clone)]
@@ -190,51 +191,6 @@ impl HostApi {
                 unit,
             },
         ))
-    }
-
-    pub fn msg_window(
-        &self,
-        event: &EventContext,
-        request: MsgWindowRequest,
-    ) -> Result<HostApiResponse<MsgWindowValue>, HostApiError> {
-        validate_event(event, HostApiOperation::MsgWindow)?;
-        self.require_operation_capability(event, HostApiOperation::MsgWindow)?;
-        validate_msg_window_request(&request, HostApiOperation::MsgWindow)?;
-
-        let messages = self
-            .storage(HostApiOperation::MsgWindow)?
-            .message_window(
-                request.chat_id,
-                request.anchor_message_id,
-                request.up,
-                request.down,
-                request.include_anchor,
-            )
-            .map_err(|source| storage_error(HostApiOperation::MsgWindow, source))?;
-
-        Ok(self.response(HostApiOperation::MsgWindow, MsgWindowValue { messages }))
-    }
-
-    pub fn msg_by_user(
-        &self,
-        event: &EventContext,
-        request: MsgByUserRequest,
-    ) -> Result<HostApiResponse<MsgByUserValue>, HostApiError> {
-        validate_event(event, HostApiOperation::MsgByUser)?;
-        self.require_operation_capability(event, HostApiOperation::MsgByUser)?;
-        validate_msg_by_user_request(&request, HostApiOperation::MsgByUser)?;
-
-        let messages = self
-            .storage(HostApiOperation::MsgByUser)?
-            .messages_by_user(
-                request.chat_id,
-                request.user_id,
-                &request.since,
-                request.limit,
-            )
-            .map_err(|source| storage_error(HostApiOperation::MsgByUser, source))?;
-
-        Ok(self.response(HostApiOperation::MsgByUser, MsgByUserValue { messages }))
     }
 
     pub fn job_schedule_after(
---
+++ src/host_api/history.rs
@@ -0,0 +1,53 @@
+use super::{
+    validation::{validate_msg_by_user_request, validate_msg_window_request},
+    HostApi, HostApiError, HostApiOperation, HostApiResponse, MsgByUserRequest, MsgByUserValue,
+    MsgWindowRequest, MsgWindowValue, storage_error, validate_event,
+};
+use crate::event::EventContext;
+
+impl HostApi {
+    pub fn msg_window(
+        &self,
+        event: &EventContext,
+        request: MsgWindowRequest,
+    ) -> Result<HostApiResponse<MsgWindowValue>, HostApiError> {
+        validate_event(event, HostApiOperation::MsgWindow)?;
+        self.require_operation_capability(event, HostApiOperation::MsgWindow)?;
+        validate_msg_window_request(&request, HostApiOperation::MsgWindow)?;
+
+        let messages = self
+            .storage(HostApiOperation::MsgWindow)?
+            .message_window(
+                request.chat_id,
+                request.anchor_message_id,
+                request.up,
+                request.down,
+                request.include_anchor,
+            )
+            .map_err(|source| storage_error(HostApiOperation::MsgWindow, source))?;
+
+        Ok(self.response(HostApiOperation::MsgWindow, MsgWindowValue { messages }))
+    }
+
+    pub fn msg_by_user(
+        &self,
+        event: &EventContext,
+        request: MsgByUserRequest,
+    ) -> Result<HostApiResponse<MsgByUserValue>, HostApiError> {
+        validate_event(event, HostApiOperation::MsgByUser)?;
+        self.require_operation_capability(event, HostApiOperation::MsgByUser)?;
+        validate_msg_by_user_request(&request, HostApiOperation::MsgByUser)?;
+
+        let messages = self
+            .storage(HostApiOperation::MsgByUser)?
+            .messages_by_user(
+                request.chat_id,
+                request.user_id,
+                &request.since,
+                request.limit,
+            )
+            .map_err(|source| storage_error(HostApiOperation::MsgByUser, source))?;
+
+        Ok(self.response(HostApiOperation::MsgByUser, MsgByUserValue { messages }))
+    }
+}
